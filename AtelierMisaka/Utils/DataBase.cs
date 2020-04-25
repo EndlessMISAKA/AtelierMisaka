9f2574f84b22a7ba4b31ad15fe7139cb9e20f686
EndlessMISAKA <kaoritokotori@gmail.com>
2020-04-25 22:11:59 +0800

    Database Func

--- AtelierMisaka/Utils/DataBase.cs
+++ AtelierMisaka/Utils/DataBase.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AtelierMisaka.Models;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.OleDb;
@@ -11,19 +12,20 @@ namespace AtelierMisaka
 
     public class Db_Base
     {
-        public static OleDbConnection conn = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Data.mdb;Persist Security Info=False;");
+        public static OleDbConnection conn = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Database.mdb;Persist Security Info=False;");
 
-        static OleDbDataAdapter oda = new OleDbDataAdapter();
-        static OleDbCommand cmd;
-        static DataSet myds;
+        //static OleDbDataAdapter oda = new OleDbDataAdapter();
+        //static OleDbCommand cmd;
+        //static DataSet myds;
 
-        public static DataSet getDS(string strSQL)
+        public static DataSet GetDS(string strSQL)
         {
+            DataSet myds = null;
             try
             {
                 conn.Open();
                 myds = new DataSet();
-                oda = new OleDbDataAdapter(strSQL, conn);
+                OleDbDataAdapter oda = new OleDbDataAdapter(strSQL, conn);
                 oda.Fill(myds);
                 oda.Dispose();
             }
@@ -36,13 +38,13 @@ namespace AtelierMisaka
             return myds;
         }
 
-        public static int setDS(string strSQL)
+        public static int SetDS(string strSQL)
         {
             int n = 0;
             try
             {
                 conn.Open();
-                cmd = new OleDbCommand(strSQL, conn);
+                OleDbCommand cmd = new OleDbCommand(strSQL, conn);
                 n = cmd.ExecuteNonQuery();
                 cmd.Dispose();
             }
@@ -58,7 +60,7 @@ namespace AtelierMisaka
 
     public class DB_Layer
     {
-        private StringBuilder sql = new StringBuilder();
+        //private StringBuilder sql = new StringBuilder();
         private DataSet ds = null;
         
         public void OpenConn()
@@ -71,6 +73,77 @@ namespace AtelierMisaka
             Db_Base.conn.Close();
         }
 
+        public Dictionary<string, List<DLSP>> GetLog(string cid)
+        {
+            Dictionary<string, List<DLSP>> result = new Dictionary<string, List<DLSP>>();
+            string sql = $"SELECT * FROM DLLogs WHERE CID='{cid}'";
+            ds = Db_Base.GetDS(sql);
+            if (null != ds)
+            {
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+                    {
+                        var ro = ds.Tables[0].Rows[i];
+                        var pid = ro["PID"].ToString();
+                        if (result.ContainsKey(pid))
+                        {
+                            result[pid].Add(new DLSP() { Savepath = ro["SavePath"].ToString(), Link = ro["DLink"].ToString() });
+                        }
+                        else
+                        {
+                            result.Add(pid, new List<DLSP>() { new DLSP() { Savepath = ro["SavePath"].ToString(), Link = ro["DLink"].ToString() } });
+                        }
+                    }
+                }
+                ds.Dispose();
+            }
+            return result;
+        }
 
+        public bool InsertLog(string cid, string pid, string savep, string link)
+        {
+            string sql = $"INSERT INTO DLLogs(CID,PID,SavePath,DLink) VALUES('{cid}','{pid}','{savep}','{link}')";
+            int re = Db_Base.SetDS(sql);
+            return re == 1;
+        }
+
+        public bool InsertDate(string cid, DateTime dt)
+        {
+            string sql = $"INSERT INTO CLast VALUES('{cid}', #{dt.ToString()}#)";
+            int re = Db_Base.SetDS(sql);
+            return re == 1;
+        }
+
+        public bool UpdateDate(string cid, DateTime dt)
+        {
+            string sql = $"UPDATE CLast SET LastDate=#{dt.ToString()}# WHERE CID='{cid}'";
+            int re = Db_Base.SetDS(sql);
+            return re == 1;
+        }
+
+        public bool? GetLastDate(string cid, out DateTime dt)
+        {
+            string sql = $"SELECT * FROM CLast WHERE CID='{cid}'";
+            ds = Db_Base.GetDS(sql);
+            dt = DateTime.MinValue;
+            if (null != ds)
+            {
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    if (DateTime.TryParse(ds.Tables[0].Rows[0]["LastDate"].ToString(), out DateTime dt1))
+                    {
+                        dt = dt1;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                ds.Dispose();
+            }
+            return null;
+        }
     }
 }
